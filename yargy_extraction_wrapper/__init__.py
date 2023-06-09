43a006c15b1c924d2f793b0e524bab62dd9e26cb
Косоурихин Алексей Александрович (яр) <a.kosourikhin@krista.ru>
2023-06-09 17:41:26 +0300

    fix init

--- yargy_extraction_wrapper/__init__.py
+++ yargy_extraction_wrapper/__init__.py
@@ -6,3 +6,6 @@ from yargy.interpretation import *
 from yargy.predicates import *
 from yargy.pipelines import *
 from yargy.rule import *
+
+from dill import pickle
+from ipymarkup import show_box_markup
